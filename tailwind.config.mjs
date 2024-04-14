acaf587711309d63e9785b3eb785c0f958e559d0
Marwin Härttrich <39224983+haerttrich@users.noreply.github.com>
2024-04-14 15:47:44 +0200

    move static files from old to astro, add defaultlayout, navbar and footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				ptSans: ['"PT Sans"', ...defaultTheme.fontFamily.sans],
+				ibarra: ['"Ibarra Real Nova"', ...defaultTheme.fontFamily.serif],
+			}
+		},
 	},
 	plugins: [],
 }
